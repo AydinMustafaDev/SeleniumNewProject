d7c2f20089d25fc330b334adccf0133c8310dbbb
AydinMustafaDev <xaydin35@gmail.com>
2023-05-26 23:38:07 +0300

    Initial commit

---
+++ src/test/java/techproed/day01_IlkTest_DriverMethods/C02_DriverMethods.java
@@ -0,0 +1,33 @@
+package techproed.day01_IlkTest_DriverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C02_DriverMethods {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://amazon.com");
+
+        //getTitle()  Sayfa başlığını verir
+        System.out.println("Amazon sayfa baslığı :"+driver.getTitle()); // methoddan önce son açılan
+                                                                         // sayfanın title'ını verir.
+        System.out.println("Amazon Actual Url :"+ driver.getCurrentUrl()); // Url verir
+        driver.get("https://techproeducation.com");
+        System.out.println("Techproed sayfa başlığı :"+ driver.getTitle());
+
+        //getCurrentUrl  Gidilen sayfanın Url'ini verir
+        System.out.println("Techproed Actual Url :"+ driver.getCurrentUrl()); // methoddan önce son
+                                                                        // açılan sayfanın title'ını verir.
+
+        //getPageSource()  Açılan sayfanın kaynak kodlarını verir.
+        // System.out.println(driver.getPageSource());    çalıştırmaya gerek yok
+
+        // getWindowHandle()  Gidilen sayfanın handle degerini(hashcode olarak) verir.
+        // Bu handle degerini sayfalar arası geciş için kullanırız
+        System.out.println("Techproed Window Handle Degeri :"+ driver.getWindowHandle());
+        //CDwindow-4C1B07A0058E38F24A7D81DD6C3FFC9F
+
+
+    }
+}
